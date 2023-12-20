b2995670f4a0ea0a4095f4aefa1d400858ba3e2e
jeryd <4661200+SurvivaLlama@users.noreply.github.com>
2023-12-20 05:48:00 -0800

    Update newtab.js

--- newtab.js
+++ newtab.js
@@ -2,13 +2,26 @@ document.addEventListener('DOMContentLoaded', function() {
     var setFocusButton = document.getElementById('setFocusButton');
     var newFocusInput = document.getElementById('newFocusInput');
     var focusText = document.getElementById('focusText');
-    var focusCheckbox = document.getElementById('focusCheckbox'); // Grab the checkbox element
+    var focusCheckbox = document.getElementById('focusCheckbox');
 
     newFocusInput.focus();
+
+    function celebrateCompletion() {
+        // Celebration logic (e.g., change background color, display a message, etc.)
+        alert('Congratulations on completing your task!'); // Example celebration
+    }
+
+    function resetFocus() {
+        focusText.style.display = 'block';
+        focusText.textContent = 'No daily focus set for today.';
+        newFocusInput.value = '';
+        focusCheckbox.checked = false;
+    }
 
     setFocusButton.addEventListener('click', function() {
         chrome.storage.local.set({ 'dailyFocus': newFocusInput.value }, function() {
             updateFocusDisplay();
+            resetFocus();
         });
     });
 
@@ -16,6 +29,7 @@ document.addEventListener('DOMContentLoaded', function() {
         if (event.key === 'Enter') {
             chrome.storage.local.set({ 'dailyFocus': newFocusInput.value }, function() {
                 updateFocusDisplay();
+                resetFocus();
             });
         }
     });
@@ -23,6 +37,7 @@ document.addEventListener('DOMContentLoaded', function() {
     focusCheckbox.addEventListener('click', function() {
         if (focusCheckbox.checked) {
             focusText.style.display = 'none';
+            celebrateCompletion();
             chrome.storage.local.set({ 'dailyFocus': '' });
         }
     });
